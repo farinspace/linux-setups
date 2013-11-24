e0b3e037650f941478ab25330ed5b52fdc761bd0
Dimas Begunoff <dbegunoff@gmail.com>
2013-11-24 07:28:28 +0000

    better apache2/vhost setup

--- apache2-install.sh
+++ apache2-install.sh
@@ -1,30 +1,20 @@
 #!/bin/bash
 
-###
-
 apt-get -yq install apache2-mpm-worker
 
-a2enmod actions
+a2enmod actions dir rewrite expires headers
 
-a2enmod dir
+a2dismod status cgid autoindex
 
-a2enmod rewrite
-
-a2enmod expires
-
-a2enmod headers
-
-a2dismod status
-
-a2dismod cgid
-
-a2dismod autoindex
+###
 
 GREPOUT=`grep "ServerName" /etc/apache2/httpd.conf`
 if [ -z "$GREPOUT" ];
 then
     echo "ServerName localhost" >> /etc/apache2/httpd.conf
 fi
+
+###
 
 ln -s /etc/apache2/sites-available /etc/apache2/sa
 
@@ -34,4 +24,32 @@ ln -s /etc/apache2/mods-available /etc/apache2/ma
 
 ln -s /etc/apache2/mods-enabled /etc/apache2/me
 
+###
+
+WWWLOGS='/etc/logrotate.d/wwwlogs'
+
+if [ ! -f $WWWLOGS ];
+then
+cat > $WWWLOGS << EOF
+/var/www/*/logs/*.log {
+        weekly
+        missingok
+        rotate 26
+        compress
+        delaycompress
+        notifempty
+        create 640 root adm
+        sharedscripts
+        postrotate
+                if [ -f "\`. /etc/apache2/envvars ; echo \${APACHE_PID_FILE:-/var/run/apache2.pid}\`" ]; then
+                        /etc/init.d/apache2 reload > /dev/null
+                fi
+        endscript
+}
+EOF
+fi
+
+###
+
 service apache2 restart
+
--- apache2-vhost.sh
+++ apache2-vhost.sh
@@ -30,8 +30,6 @@ mkdir /var/www/$SHORTNAME/html
 
 mkdir /var/www/$SHORTNAME/logs
 
-echo '<?php phpinfo(); ?>' > /var/www/$SHORTNAME/html/index.php
-
 cat > /etc/apache2/sites-available/$SHORTNAME << EOF
 <VirtualHost *:80>
   ServerName $HOSTNAME
@@ -49,27 +47,5 @@ EOF
 
 ln -s /etc/apache2/sites-available/$SHORTNAME /etc/apache2/sites-enabled/$SHORTNAME
 
-WWWLOGS='/etc/logrotate.d/wwwlogs'
-
-if [ ! -f $WWWLOGS ];
-then
-cat > $WWWLOGS << EOF
-/var/www/*/logs/*.log {
-	weekly
-	missingok
-	rotate 26
-	compress
-	delaycompress
-	notifempty
-	create 640 root adm
-	sharedscripts
-	postrotate
-		if [ -f "\`. /etc/apache2/envvars ; echo \${APACHE_PID_FILE:-/var/run/apache2.pid}\`" ]; then
-			/etc/init.d/apache2 reload > /dev/null
-		fi
-	endscript
-}
-EOF
-fi
-
 service apache2 reload
+
