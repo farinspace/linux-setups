c3c1eb04df4ae193561396cc025fa3be9178bb40
farinspace <dbegunoff@gmail.com>
2013-11-24 06:28:56 +0000

    better LAMP scripts

--- php-install.sh
+++ php-install.sh
@@ -1,133 +1,37 @@
 #!/bin/bash
 
-###
+# http://askubuntu.com/questions/109404/how-do-i-install-latest-php-in-supported-ubuntu-versions-like-5-4-x-in-ubuntu-1
+add-apt-repository -y ppa:ondrej/php5
 
-if ! which expect &> /dev/null;
-then
-    echo "Unable to find command 'expect', run ./essentials.sh first"
-    exit
-fi;
+apt-get -yq update
 
-###
-
-echo -n "Web user account [default=www-data]: "
-read USERNAME
-
-if [ -z "$USERNAME" ]
-then
-    USERNAME='www-data'
-fi
+apt-get -yq update
 
 ###
 
-if [ "$USERNAME" != "www-data" ]
-then
-	echo -n "Web user account password (leave blank for none): "
-	read -s PASSWORD
-	echo ""
+apt-get -yq install libapache2-mod-fastcgi php5-fpm php5
 
-	if [ ! -z "$PASSWORD" ]
-	then
-		echo -n "Retype web user account password: "
-		read -s PASSWORD2
-		echo ""
+apt-get -yq install php-pear php5-cli php5-mysql php5-gd php5-imagick php5-curl php5-tidy php5-xmlrpc php5-mcrypt php5-sqlite php5-recode php5-pspell
 
-		if [ "$PASSWORD" != "$PASSWORD2" ]
-		then
-			echo "Web user account passwords do not match"
-			exit
-		fi
-	fi
+apt-get -yq install php5-memcache memcached
 
-	###
+a2enmod actions fastcgi alias
 
-	useradd --home /var/www -M $USERNAME
+###
 
-	if [ ! -z "$PASSWORD" ]
-	then
-
-# add user password
-expect << EOF
-spawn passwd $USERNAME
-expect "Enter new UNIX password:"
-send "${PASSWORD}\r" 
-expect "Retype new UNIX password:"
-send "${PASSWORD}\r"
-expect eof;
+cat > /etc/apache2/conf.d/php5-fpm.conf << EOF
+DirectoryIndex index.php index.html
+<IfModule mod_fastcgi.c>
+  AddHandler php5-fcgi .php
+  Action php5-fcgi /php5-fcgi
+  Alias /php5-fcgi /usr/lib/cgi-bin/php5-fcgi
+  FastCgiExternalServer /usr/lib/cgi-bin/php5-fcgi -socket /var/run/php5-fpm.sock -pass-header Authorization
+</IfModule>
 EOF
-
-	fi
-fi
-
-###
-
-add-apt-repository ppa:brianmercer/php
-
-apt-get -yq update
-
-apt-get -yq update
-
-apt-get -yq install php5-fpm
 
 ###
 
 sed -e "s/;\?expose_php .*/expose_php \= Off/g" -i /etc/php5/fpm/php.ini
-
-###
-
-sleep 2
-
-###
-
-mkdir /var/www/conf
-
-mkdir /var/www/logs
-
-mkdir /var/www/temp
-
-###
-
-FPMCONF='/etc/php5/fpm/php5-fpm.conf'
-
-mv $FPMCONF "${FPMCONF}.bak"
-
-cat > $FPMCONF << EOF
-; global options
-[global]
-pid = /var/run/php5-fpm.pid
-error_log = /var/log/php5-fpm.log
-
-; default pool
-[default]
-listen = /etc/php5/fpm/php5-fpm.sock
-listen.owner = $USERNAME
-listen.group = $USERNAME
-user = $USERNAME
-group = $USERNAME
-pm = dynamic
-pm.max_children = 3
-pm.start_servers = 2
-pm.min_spare_servers = 2
-pm.max_spare_servers = 3
-pm.max_requests = 500
-slowlog = /var/log/php5-fpm.slow.log
-env[PATH] = /usr/local/bin:/usr/bin:/bin
-env[TMP] = /var/www/temp
-env[TMPDIR] = /var/www/temp
-env[TEMP] = /var/www/temp
-
-; pool options
-; include=/var/www/*/conf/fpm-pool.conf
-EOF
-
-###
-
-# not installing php5-cgi, will auto install apache
-apt-get -yq install php-pear php5-cgi php5-cli php5-mysql php5-gd php5-imagick php5-curl php5-tidy php5-xmlrpc php5-mcrypt
-
-apt-get -yq install php5-memcache
-
-apt-get -yq install memcached
 
 sed -e "s/^#\+/;/g" -i /etc/php5/fpm/conf.d/imagick.ini
 
@@ -135,26 +39,7 @@ sed -e "s/^#\+/;/g" -i /etc/php5/fpm/conf.d/mcrypt.ini
 
 ###
 
-chown -R $USERNAME:$USERNAME /var/www
-
-HTTPDCONF='/etc/apache2/httpd.conf'
-
-if [ -f $HTTPDCONF ]
-then
-	echo "Include /var/www/conf/php5-fpm.conf" >> $HTTPDCONF
-
-cat > /var/www/conf/php5-fpm.conf << EOF
-DirectoryIndex index.php index.html
-<IfModule mod_fastcgi.c>
-  Alias /php.fcgi /var/www/php.fcgi
-  FastCGIExternalServer /var/www/php.fcgi -socket /etc/php5/fpm/php5-fpm.sock -pass-header Authorization
-  AddType application/x-httpd-php5 .php
-  Action application/x-httpd-php5 /php.fcgi
-  AddHandler fastcgi-script .fcgi
-</IfModule>
-EOF
-fi
-
-service php5-fpm start
+service php5-fpm restart
 
 service apache2 restart
+
