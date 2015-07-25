e7ae659d62512701ecfb34e9f0db39bbbca5cf58
Dimas Begunoff <dbegunoff@gmail.com>
2015-07-25 14:33:08 -0700

    added ability to add publickey

--- add-sftp-user.sh
+++ add-sftp-user.sh
@@ -2,7 +2,7 @@
 
 ###
 
-echo -n "Username: (e.g. john-sftp)"
+echo -n "Username: [e.g. john-sftp] "
 read USERNAME
 
 if [ -z "$USERNAME" ]
@@ -13,6 +13,27 @@ fi
 
 ###
 
+echo -n "SSH Public Key, e.g. \"ssh-rsa [PUBLICKEY]\" (end input with ESC):"
+read -d `echo -e "\e"` PUBLICKEY
+echo ""
+
+if [ -z "$PUBLICKEY" ]
+then
+    echo "Public Key must not be empty"
+    exit
+fi
+
+###
+
 groupadd -f sftp
 
 useradd -g sftp -s /sbin/nologin -m $USERNAME
+
+###
+
+mkdir /home/$USERNAME/.ssh
+
+touch /home/$USERNAME/.ssh/authorized_keys
+
+# remove newlines from public key
+echo "$PUBLICKEY" | sed ':a;N;$!ba;s/\n//g' >> /home/$USERNAME/.ssh/authorized_keys
